7594d345047a3f73743ede724ad109cac498c3c5
Allen Lancelot <alan910810@gmail.com>
2016-01-03 18:35:08 +0800

    version 1.0

--- webpage_scrapy.py
+++ webpage_scrapy.py
@@ -15,13 +15,16 @@ def getNames():
 
         else:
             # Initialise the BeautifulSoup
+
             bsObj = BeautifulSoup(html.read(), "html.parser")
 
             # Find bad content  : where nonExistentTag is a made-up tag, not the
             # name of a real BeautifulSoup function :
             # bsObj.findAll(tagName, tagAttributes)
+
             try:
                 # bsObj.tag.subTag.anotherSubTag
+
                 content = bsObj.findAll("span", {"class": "green"})
 
             except AttributeError as e:
@@ -32,12 +35,14 @@ def getNames():
                     print("Tag was not found")
                 else:
                     #  Get All names excepts "the prince" and "The prince"
+
                     resultList = list(
                         filter(lambda x: x.get_text() not in "the prince"
                                and x.get_text() not in "The prince", content))
                     print([name.get_text() for name in resultList])
 
                     # get Total no. of "the prince "
+
                     princeList = bsObj.findAll("", {"id": "text"})
 
                     print(
@@ -51,7 +56,20 @@ def getNames():
 
 
 def getTableElement():
-    raise
 
+    # Initialise the BeautifulSoup
+
+    html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+    bsObj = BeautifulSoup(html, "html.parser")
+
+    # Scan the table by cursor -> bsObj.find("table", {"id":
+    # "giftList"}).tr.next_siblings
+
+    for sibling in bsObj.find("table", {"id": "giftList"}).tr.next_siblings:
+        print(sibling)
+
+    print(bsObj.find("img", {"src": "../img/gifts/img2.jpg"}) .parent.previous_sibling.previous_sibling.previous_sibling.get_text()
+          + "Price :  "
+          + bsObj.find("img", {"src": "../img/gifts/img2.jpg"}).parent.previous_sibling.get_text())
 if __name__ == '__main__':
-    getNames()
+    getTableElement()
