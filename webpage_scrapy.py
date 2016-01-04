05d1440fb277cd2b8ed85b77f81b2671baba0d13
Allen Lancelot <alan910810@gmail.com>
2016-01-05 00:36:41 +0800

    version 2.2
    Add more crawling cases :
    1) Crawling entire site
    2) traverSingleDomain
    3) traverSingleDomain2

--- webpage_scrapy.py
+++ webpage_scrapy.py
@@ -137,11 +137,27 @@ def getLinks(articleUrl):
     return bsObj.find("div", {"id": "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
 
 
+def crawlEntireSite(pageUrl):
+    pages = set()
+    html = urlopen("http://en.wikipedia.org" + pageUrl)
+    bsObj = BeautifulSoup(html, "html.parser")
+    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                # We have encountered a new page
+                newPage = link.attrs['href']
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+
+
 if __name__ == '__main__':
+
     # getNames()
     # getTableElement()
     # getContentByRegularExpression()
     # getAllAttributesByLambdaExp()
-
     # traverSingleDomain()
-    traverSingleDomain2()
+    # traverSingleDomain2()
+
+    crawlEntireSite("")
